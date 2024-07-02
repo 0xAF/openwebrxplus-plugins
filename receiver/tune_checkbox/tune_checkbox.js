38cc7961111d31d4c913dd80d3b5adec051ded08
LZ2DMV <81835377+LZ2DMV@users.noreply.github.com>
2024-07-02 11:16:18 +0300

    Update tune_checkbox.js

--- receiver/tune_checkbox/tune_checkbox.js
+++ receiver/tune_checkbox/tune_checkbox.js
@@ -1,7 +1,10 @@
 /*
  * Plugin: Make the 'Hold mouse wheel down to tune' option checked by default.
  * Allows you to zoom into the waterfall with the mouse scroll.
-
+ *
+ * By default, the state of this checkbox is stored in localStorage in your browser.
+ * If you often delete your browser's cache and localStorage contents, this plugin might be useful.
+ *
  * License: Apache License 2.0
  * Copyright (c) 2024 Dimitar Milkov, LZ2DMV
  */
