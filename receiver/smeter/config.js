3f528b5c79061868371996d3a214311119308e1e
joer123 <87607501+joer123@users.noreply.github.com>
2026-01-01 23:04:07 +0100

    Update smeter configuration values

--- receiver/smeter/config.js
+++ receiver/smeter/config.js
@@ -3,8 +3,8 @@
  */
 
 window.smeter_config_global = {
-    calibration_offset_hf: -40,  // Calibration for HF (<30MHz) in dB
-    calibration_offset_vhf: -10, // Calibration for VHF/UHF (>30MHz) in dB
-    hide_original: true,        // Set to true to hide the original S-meter
-    show_text: true             // Set to false to hide the text below the S-meter
+    calibration_offset_hf: 0,  // Calibration for HF (<30MHz) in dB
+    calibration_offset_vhf: 0, // Calibration for VHF/UHF (>30MHz) in dB
+    hide_original: false,        // Set to true to hide the original S-meter
+    show_text: false             // Set to false to hide the text below the S-meter
 };
